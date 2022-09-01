530f2329d769b9f8a78a3a57f1dcb1a605856854
peetacho <peterchow2001@gmail.com>
2022-09-01 00:14:50 -0400

    added working file transfer & download

--- client/src/components/Room/Room.js
+++ client/src/components/Room/Room.js
@@ -5,8 +5,30 @@ import {
 import { useSocket } from "../../contexts/SocketProvider";
 import { useParams } from "react-router-dom";
 
+const downloadArrayBufferAsFile = (arrayBuffer, mimType, fileName) => {
+    let binary = '';
+    const bytes = new Uint8Array(arrayBuffer);
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+    }
+    const file = window.btoa(binary);
+    const url = `data:${mimType};base64,` + file;
+
+    // download the file
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+}
+
 const Room = () => {
     const [messages, setMessages] = useState([])
+    const [fileData, setFileData] = useState(null)
+    const [receivedFileData, setReceivedFileData] = useState(null)
     const inputRef = useRef(null);
     const { roomID } = useParams();
     const socket = useSocket();
@@ -15,10 +37,10 @@ const Room = () => {
         setMessages([...messages, { msg: msg, sender: sender }])
     }, [messages])
 
-    const receiveMessage = useCallback(({ msg, sender }) => {
+    const receiveMessage = useCallback(({ msg, sender, fileData }) => {
         addMessage(msg, sender)
+        setReceivedFileData(fileData)
     }, [addMessage])
-
 
     useEffect(() => {
         if (socket === null) return
@@ -37,11 +59,24 @@ const Room = () => {
                 ref={inputRef}
             />
             <Button onClick={e => {
-                socket.emit("send-message", { msg: inputRef.current.value, room: roomID })
+                socket.emit("send-message", { msg: inputRef.current.value, room: roomID, fileData: fileData })
                 addMessage(inputRef.current.value, "You")
             }}>
                 Send msg
             </Button>
+            <Input type="file" onChange={e => {
+                let targetFile = e.target.files[0]
+                if (targetFile) {
+                    setFileData({ file: targetFile, fileType: targetFile.type, fileName: targetFile.name });
+                }
+                // console.log(e.target.files[0])
+            }} />
+            {
+                receivedFileData ? (
+                    // <Image src={URL.createObjectURL(new Blob([fileArrayBuffer]))} />
+                    <Button onClick={() => downloadArrayBufferAsFile(receivedFileData.file, receivedFileData.fileType, receivedFileData.fileName)}></Button>
+                ) : null
+            }
             <Stack>
                 {messages && messages.map((message, i) => {
                     return (
--- server/server.js
+++ server/server.js
@@ -1,23 +1,18 @@
 const io = require('socket.io')(8080, {
     cors: {
         origin: ["http://localhost:3000"]
-    }
+    },
+    maxHttpBufferSize: 1e9
 })
 
 io.on('connection', socket => {
-    console.log(socket.id + "has connected!")
-
     socket.on("join-room", room => {
-        console.log(socket.id + " has joined the room: " + room)
         socket.join(room)
     })
 
-    socket.on("send-message", ({ msg, room }) => {
-        console.log(socket.id + " has sent a message to the room: " + room)
+    socket.on("send-message", ({ msg, room, fileData }) => {
         if (room !== "") {
-            socket.to(room).emit("receive-message", { msg: msg, sender: socket.id })
-            console.log("everyone else will now receieve a message from " + socket.id + " to the room: " + room)
+            socket.to(room).emit("receive-message", { msg: msg, sender: socket.id, fileData: fileData })
         }
     })
-
 })
